295bd4da30590e3ae9fbcb718fffd18e2d20e529
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-06-28 18:50:47 +0530

    feat: add function to fetch logged in user details

--- utils/user.ts
+++ utils/user.ts
@@ -1,7 +1,39 @@
+import AxiosInstance from './axiosInstance';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { toast } from 'sonner';
-import AxiosInstance from './axiosInstance';
 import { resetLocalStorage } from '@/lib/storage';
+import { User } from '@/context/user.context';
+import { AxiosError } from 'axios';
+
+const fetchUser = async (): Promise<User> => {
+    try {
+        const response = await AxiosInstance.get(`/api/v1/user`, {
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.data.success) {
+            throw new Error(response.data.message || 'Failed to fetch user details');
+        }
+        return response.data.data;
+    } catch (err) {
+        const error = err as AxiosError;
+        const statusCode = error.response?.status;
+
+        if (error.response && statusCode === 404) {
+            throw new Error((error.response.data as AxiosError)?.message || 'User not found');
+        }
+        if (error.response) {
+            throw new Error((error.response.data as AxiosError)?.message || 'Failed to fetch user details');
+        } else if (error.request) {
+            throw new Error('No response from server. Check your network.');
+        } else {
+            throw new Error('Unexpected error occurred');
+        }
+    }
+};
 
 const handleLogout = async (router: AppRouterInstance) => {
     try {
@@ -24,4 +56,4 @@ const handleLogout = async (router: AppRouterInstance) => {
     }
 };
 
-export { handleLogout };
+export { fetchUser, handleLogout };
