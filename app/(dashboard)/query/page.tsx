f57a544864fa90c41a0c93fa29ecbca93a03529d
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-07-05 16:55:12 +0530

    fix: wrap Queries in suspense

--- app/(dashboard)/query/page.tsx
+++ app/(dashboard)/query/page.tsx
@@ -1,10 +1,17 @@
 import { Metadata } from 'next';
 import Queries from '@/components/Query/Queries';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
     title: 'Query',
 };
 
 export default function Page() {
-    return <Queries />;
+    return (
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Queries />
+            </Suspense>
+        </>
+    );
 }
