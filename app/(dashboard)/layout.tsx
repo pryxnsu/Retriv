fafb639a556e943bec9c5bdcbc18a9beeb0c8121
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-12-01 15:39:29 +0530

    refactor: wrap Sidebar in Suspense

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import MobileNavbar from '@/components/MobileNavbar';
 import Sidebar from '@/components/Sidebar';
 import { UserProvider } from '@/context/user.context';
+import { Suspense } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
@@ -9,7 +10,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 {/* Sidebar */}
                 <nav className="hidden lg:block">
                     <aside className="w-64 flex flex-col justify-between h-screen border-r border-neutral-300 dark:border-neutral-800 p-4">
-                        <Sidebar />
+                        <Suspense fallback={<div />}>
+                            <Sidebar />
+                        </Suspense>
                     </aside>
                 </nav>
 
